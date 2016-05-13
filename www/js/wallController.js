51b344f3457f6c728197ef94fe89287849ff7686
Mihael Grilec <mihael@mgrilec.com>
2016-05-13 22:32:30 +0200

    Added infinite scroll.

--- www/js/wallController.js
+++ www/js/wallController.js
@@ -3,13 +3,17 @@ angular.module('gifapp.controllers', []).
     controller('wallController', function ($scope, giphyAPIservice) {
         $scope.gifs = [];
 
-        // request gifs
-        var refresh = function() {
-            giphyAPIservice.getGifs('cute+animals', 25, 0).success(function (response) {
-                $scope.gifs = response.data;
+        $scope.loadMore = _.throttle(function() {
+        	giphyAPIservice.getGifs('cute+animals', 10, $scope.gifs.length).success(function (response) {
+
+            	_.each(response.data, function(data) {
+            		$scope.gifs.push(data);
+            	});
 
                 console.log(response);
+                $scope.$broadcast('scroll.infiniteScrollComplete');
             });
-        };
-        refresh();
+        }, 3000);
+
+        $scope.loadMore();
     });
